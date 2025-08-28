f2da9e4448c7079d9b77b831963f7ee5985473c1
abreham <abrehamnigus1996@gmail.com>
2025-08-28 17:27:31 +0300

    first commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
-
-function Header() {
+import './header.css'
+import NetflixLogo from '../../assets/images/Netflix Logo.png';
+const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className='header_outer_container'>
+      <div className='header_container'>
+    <div className='header_left'>
+      <ul>
+        <li><img src={NetflixLogo} alt="Netflix Logo" width="100" /></li>
+        
+        <li>Home</li>
+        <li>TVShows</li>
+        <li>Movies</li>
+        <li>Latest</li>
+        <li>MyList</li>
+        <li>Browse by Languages</li>
+      </ul>
+    </div>
 
-export default Header
+    <div className='header_right'>
+      <ul>
+        {/* <li><SearchIcon /></li>
+        <li><NotificationsNoneIcon /></li>
+        <li><AccountBoxIcon /></li>
+        <li><ArrowDropDownIcon /></li> */}
+      </ul>
+    </div>
+  </div>
+   </div>
+);
+
+};
+
+export default Header;
