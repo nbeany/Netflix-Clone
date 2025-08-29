03d00fbaf2159e80637a95e772f76ba0f6d53bd2
abreham <abrehamnigus1996@gmail.com>
2025-08-29 11:02:21 +0300

    api

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './Banner.css'
+import axios from '../../utils/axios';
+import requests from '../../utils/request';
+function Banner() {
+    const [movie, setMovie] = React.useState({});
+            useEffect(() => {
+                (async () => {
+                    try {
+                        const request = await axios.get(requests.fetchNetflixOriginals);
+                        setMovie(request.data.results[
+                            Math.floor(Math.random() * request.data.results.length)
+                        ]);
+                    } catch (error) {
+                        console.log("error", error);
+                    }
+                })();
+            }, []);
+  return (
+  <div
+    className="banner"
+    style={{
+      backgroundSize: "cover",
+      backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+    }}
+  >
+    <div className="banner__contents">
+      <h1 className="banner__title">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <div className="banner__buttons">
+        <button className="banner__button play">Play</button>
+        <button className="banner__button">My List</button>
+      </div>
+      <h1 className="banner__description">{truncate(movie?.overview, 150)}</h1>
+    </div>
+    <div className="banner__fadeBottom" />
+  </div>
+
+  );
+}
+
+export default Banner
